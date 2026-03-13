eb569f2c9b3c05808fd5c38b6b4db0a016f2aed7
Colo1013 <ale1013.coletta@gmail.com>
2026-03-13 15:27:04 +0100

    anello timer

--- lib/features/focus_dojo/focusdojo_view.dart
+++ lib/features/focus_dojo/focusdojo_view.dart
@@ -1,10 +1,22 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mangaflow/features/focus_dojo/widgets/exp_pill.dart';
 import 'package:mangaflow/features/focus_dojo/widgets/focus_ring_painter.dart';
 import 'package:mangaflow/theme/app_sizes.dart';
 
-class FocusdojoView extends StatelessWidget {
+class FocusdojoView extends ConsumerStatefulWidget {
   const FocusdojoView({super.key});
+
+  @override
+  ConsumerState<FocusdojoView> createState() => _FocusdojoViewState();
+}
+
+class _FocusdojoViewState extends ConsumerState<FocusdojoView> {
+  Duration durata = Duration();
+  static const Duration durataMax = Duration(hours: 1);
+  final _ringKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -15,22 +27,55 @@ class FocusdojoView extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Padding(
-              padding: EdgeInsets.only(top: appSizes.small),
-              child: ExpPill(
-                data: ExpMockData(
-                  grado: "Novizio",
-                  expAttuali: 150,
-                  expTotali: 300,
-                  coloreSfondo: Colors.blue,
-                ),
+            SizedBox(height: 16),
+            ExpPill(
+              data: ExpMockData(
+                grado: "Novizio",
+                expAttuali: 150,
+                expTotali: 300,
+                coloreSfondo: Colors.blue,
               ),
             ),
+            SizedBox(height: 16),
             Expanded(
-              child: SizedBox(
-                height: 300,
-                width: 200,
-                child: CustomPaint(painter: FocusRingPainter()),
+              child: GestureDetector(
+                onPanUpdate: (details) {
+                  final size =
+                      (_ringKey.currentContext!.findRenderObject() as RenderBox)
+                          .size;
+                  final dx = details.localPosition.dx - size.width / 2;
+                  final dy = details.localPosition.dy - size.height / 2;
+                  final angolo = atan2(dy, dx);
+                  final progressione = (angolo + pi / 2) % (2 * pi) / (2 * pi);
+                  setState(() {
+                    final minuti = (durataMax.inSeconds * progressione / 60)
+                        .round();
+                    durata = Duration(minutes: minuti);
+                  });
+                  print(
+                    "${details.localPosition} ; ${size}; progressione ${progressione}",
+                  );
+                },
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    AspectRatio(
+                      key: _ringKey,
+                      aspectRatio: 1,
+                      child: CustomPaint(
+                        painter: FocusRingPainter(
+                          progressione: durata.inSeconds / durataMax.inSeconds,
+                        ),
+                      ),
+                    ),
+
+                    Text(
+                      durata.inMinutes == 0
+                          ? "∞"
+                          : "${durata.inMinutes} : ${durata.inSeconds % 60}",
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
--- lib/features/focus_dojo/widgets/focus_ring_painter.dart
+++ lib/features/focus_dojo/widgets/focus_ring_painter.dart
@@ -5,6 +5,9 @@ class FocusRingPainter extends CustomPainter {
   final double strokeWidth = 2.0;
   final int dashStep = 4; // Disegna un trattino ogni 4 gradi
   final double dashLength = 15.0; // Quanto è lungo ogni singolo trattino
+  final double progressione;
+
+  FocusRingPainter({required this.progressione});
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -20,9 +23,14 @@ class FocusRingPainter extends CustomPainter {
       ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke;
 
+    final paint2 = Paint()
+      ..color = Colors.redAccent.withValues(alpha: 0.3)
+      ..strokeWidth = strokeWidth
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke;
     // 4. LA MATEMATICA: Un ciclo for che fa un giro completo (360 gradi)
     for (int i = 0; i < 360; i += dashStep) {
-      final double radians = i * (pi / 180);
+      final double radians = i * (pi / 180) - (pi / 2);
 
       // Calcoliamo il punto di INIZIO del trattino (raggio interno)
       final double innerRadius = radius - dashLength;
@@ -32,13 +40,16 @@ class FocusRingPainter extends CustomPainter {
       // Calcoliamo il punto di FINE del trattino (raggio esterno)
       final double x2 = center.dx + radius * cos(radians);
       final double y2 = center.dy + radius * sin(radians);
-
-      canvas.drawLine(Offset(x1, y1), Offset(x2, y2), paint);
+      if (i / 360 > progressione) {
+        canvas.drawLine(Offset(x1, y1), Offset(x2, y2), paint);
+      } else {
+        canvas.drawLine(Offset(x1, y1), Offset(x2, y2), paint2);
+      }
     }
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
+    return (oldDelegate as FocusRingPainter).progressione != progressione;
   }
 }
