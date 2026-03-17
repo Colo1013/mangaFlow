ac43077a150fd59d1585598ccc4c1d5f315eba37
Colo1013 <ale1013.coletta@gmail.com>
2026-03-17 16:18:27 +0100

    fix

---
+++ lib/data/models/profile.dart
@@ -0,0 +1,39 @@
+class Profile {
+  final int totalExp;
+  final String userName;
+  final int id = 1;
+  static const Map<int, int> levelMap = {
+    1: 0,
+    2: 100,
+    3: 200,
+    4: 350,
+    5: 680,
+    6: 1300,
+  };
+
+  Profile({required this.totalExp, required this.userName, id});
+
+  int get level {
+    int out = 0;
+    levelMap.entries.forEach((entry) {
+      out = entry.value <= totalExp ? entry.key : out;
+    });
+    return out;
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      "totalExp": totalExp,
+      "userName": userName,
+      "id": id,
+    };
+  }
+
+  factory Profile.fromMap(Map<String, dynamic> map) {
+    return Profile(
+      totalExp: map["totalExp"],
+      userName: map["userName"],
+      id: map["id"],
+    );
+  }
+}
---
+++ lib/data/models/session.dart
@@ -0,0 +1,45 @@
+class Session {
+  final int startTimestamp;
+  final int endTimestamp;
+  final int mangaId;
+  final int expGained;
+
+  Session({
+    required this.startTimestamp,
+    required this.endTimestamp,
+    required this.mangaId,
+    required this.expGained,
+  });
+
+  Session copyWith({
+    int? startTimestamp,
+    int? endTimestamp,
+    int? mangaId,
+    int? expGained,
+  }) {
+    return Session(
+      startTimestamp: startTimestamp ?? this.startTimestamp,
+      endTimestamp: endTimestamp ?? this.endTimestamp,
+      mangaId: mangaId ?? this.mangaId,
+      expGained: expGained ?? this.expGained,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      "startTimestamp": startTimestamp,
+      "endTimestamp": endTimestamp,
+      "mangaId": mangaId,
+      "expGained": expGained,
+    };
+  }
+
+  factory Session.fromMap(Map<String, dynamic> map) {
+    return Session(
+      startTimestamp: map["startTimestamp"],
+      endTimestamp: map["endTimestamp"],
+      mangaId: map["mangaId"],
+      expGained: map["expGained"],
+    );
+  }
+}
